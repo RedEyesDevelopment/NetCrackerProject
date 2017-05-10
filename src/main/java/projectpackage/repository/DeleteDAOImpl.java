0fae1e021b48e4f6006b91853de94908eb189347
AlexMerlyan <sasha-merlyan@yandex.com>
2017-05-11 01:12:21 +0300

    NET CRACKER PROJECT. Version: 0.0.4 Branch: deleteDAO Date: 11.05.2017 Author: AlexMerlyan
    Working: Delete DAO
    Changes: sql function created
    Next: tests- sql exception in method deleteSingleEntityId
    
    Comment:
    function

--- src/main/java/projectpackage/repository/DeleteDAOImpl.java
+++ src/main/java/projectpackage/repository/DeleteDAOImpl.java
@@ -2,7 +2,14 @@ package projectpackage.repository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.SqlOutParameter;
+import org.springframework.jdbc.core.SqlParameter;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcCall;
 import org.springframework.stereotype.Repository;
+
+import java.sql.Types;
+import java.util.Map;
 
 /**
  * Created by Arizel on 09.05.2017.
@@ -15,20 +22,14 @@ public class DeleteDAOImpl implements DeleteDAO{
 
     @Override
     public int deleteSingleEntityById(int id) {
-        jdbcTemplate.execute("DELETE FROM OBJECTS WHERE OBJECTS.OBJECT_ID="+id);
-        return 0;
-    }
+        SimpleJdbcCall call = new SimpleJdbcCall(jdbcTemplate)
+                .withFunctionName("DELETE_OBJECT_BY_ID")
+                .declareParameters(
+                        new SqlParameter("obj_id", Types.BIGINT),
+                        new SqlOutParameter("count_rows", Types.BIGINT));
 
-//    @Override
-//    public int deleteSingleEntityById(int id) {
-//        SimpleJdbcCall call = new SimpleJdbcCall(jdbcTemplate)
-//                .withFunctionName("DELETE_OBJECT_BY_ID")
-//                .declareParameters(
-//                        new SqlParameter("obj_id", Types.BIGINT),
-//                        new SqlOutParameter("count_rows", Types.BIGINT));
-//
-//        Map<String, Object> execute = call.execute(new MapSqlParameterSource("obj_id", id));
-//        jdbcTemplate.execute("Rollback");
-//        return Math.toIntExact((Long) execute.get("count_rows"));
-//    }
+        Map<String, Object> execute = call.execute(new MapSqlParameterSource("obj_id", id));
+        //jdbcTemplate.execute("Rollback");
+        return Math.toIntExact((Long) execute.get("count_rows"));
+    }
 }
--- src/sql/functions.sql
+++ src/sql/functions.sql
@@ -1,8 +1,15 @@
+SET SERVEROUTPUT ON;
+
 CREATE OR REPLACE FUNCTION delete_object_by_id(obj_id IN NUMBER)
 RETURN NUMBER
-IS count_rows NUMBER;
+IS
+ref_count NUMBER;
+count_rows NUMBER;
+e exception;
 BEGIN
     count_rows := 0;
+    SELECT COUNT(*) INTO ref_count FROM OBJREFERENCE WHERE REFERENCE = obj_id;
+    IF ref_count > 0 THEN RAISE e;END IF;
     EXECUTE IMMEDIATE 'DELETE FROM OBJREFERENCE WHERE OBJECT_ID = ' || obj_id;
     count_rows := count_rows + SQL%ROWCOUNT;
     EXECUTE IMMEDIATE 'DELETE FROM ATTRIBUTES WHERE OBJECT_ID = ' || obj_id;
@@ -10,4 +17,10 @@ BEGIN
     EXECUTE IMMEDIATE 'DELETE FROM OBJECTS WHERE OBJECT_ID = ' || obj_id;
     count_rows := count_rows + SQL%ROWCOUNT;
     RETURN count_rows;
+EXCEPTION WHEN e THEN
+RAISE_APPLICATION_ERROR(-20001, 'Cannot delete object with references on self!');
+END;
+
+BEGIN
+DBMS_OUTPUT.PUT_LINE(delete_object_by_id(5));
 END;
