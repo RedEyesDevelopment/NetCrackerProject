e6f677e2540e062c66b27486f867a79dccb61726
Sergey Alekseenko <alekseenko365@gmail.com>
2017-05-18 01:47:19 +0300

    NET CRACKER PROJECT. Version: 0.0.2 Branch: alekseenko_dao Date: 18.05.2017 Author: alekseenkoSergey
    Working: added DAO to NotificationType, little refactor NotificationDAO
    Changes: NotificationTypeDAOImpl, NotificationDAOImpl
    Next: make service to NotificationDAO
    
    Comment:

--- src/main/java/projectpackage/repository/notificationsdao/NotificationDAOImpl.java
+++ src/main/java/projectpackage/repository/notificationsdao/NotificationDAOImpl.java
@@ -18,7 +18,8 @@ public class NotificationDAOImpl extends AbstractDAO implements NotificationDAO{
     public int insertNotification(Notification notification) throws TransactionException {
         Integer objectId = nextObjectId();
         try {
-            jdbcTemplate.update(insertObjects, objectId, null, 4, null, null);  //4 = Notification
+            //4 = Notification
+            jdbcTemplate.update(insertObjects, objectId, null, 4, null, null);
             //22 = Message  23 = Send_date  25 = Executed_date
             jdbcTemplate.update(insertAttributes, 22, objectId, notification.getMessage(), null);
             jdbcTemplate.update(insertAttributes, 23, objectId, null, notification.getSendDate());
@@ -38,31 +39,32 @@ public class NotificationDAOImpl extends AbstractDAO implements NotificationDAO{
     public void updateNotification(Notification newNotification, Notification oldNotification) throws TransactionException {
         try {
             if (!oldNotification.getMessage().equals(newNotification.getMessage())) {
-                jdbcTemplate.update(updateAttributes, newNotification.getMessage(), null, newNotification.getObjectId(), 22);
+                jdbcTemplate.update(updateAttributes, newNotification.getMessage(), null,
+                        newNotification.getObjectId(), 22);
             }
             if (!oldNotification.getSendDate().equals(newNotification.getSendDate())) {
-                jdbcTemplate.update(updateAttributes, null, newNotification.getSendDate(), newNotification
-                        .getObjectId(), 23);
+                jdbcTemplate.update(updateAttributes, null, newNotification.getSendDate(),
+                        newNotification.getObjectId(), 23);
             }
             if (!oldNotification.getExecutedDate().equals(newNotification.getExecutedDate())) {
-                jdbcTemplate.update(updateAttributes, null, newNotification.getExecutedDate(), newNotification
-                        .getObjectId(), 25);
+                jdbcTemplate.update(updateAttributes, null, newNotification.getExecutedDate(),
+                        newNotification.getObjectId(), 25);
             }
             if (oldNotification.getAuthor().getObjectId() != newNotification.getAuthor().getObjectId()) {
-                jdbcTemplate.update(updateReference, newNotification.getAuthor().getObjectId(), newNotification
-                        .getObjectId(), 21);
+                jdbcTemplate.update(updateReference, newNotification.getAuthor().getObjectId(),
+                        newNotification.getObjectId(), 21);
             }
             if (oldNotification.getNotificationType().getObjectId() != newNotification.getNotificationType().getObjectId()) {
-                jdbcTemplate.update(updateReference, newNotification.getNotificationType().getObjectId(), newNotification
-                        .getObjectId(), 26);
+                jdbcTemplate.update(updateReference, newNotification.getNotificationType().getObjectId(),
+                        newNotification.getObjectId(), 26);
             }
             if (oldNotification.getExecutedBy().getObjectId() != newNotification.getExecutedBy().getObjectId()) {
-                jdbcTemplate.update(updateReference, newNotification.getExecutedBy().getObjectId(), newNotification
-                        .getObjectId(), 24);
+                jdbcTemplate.update(updateReference, newNotification.getExecutedBy().getObjectId(),
+                        newNotification.getObjectId(), 24);
             }
             if (oldNotification.getOrder().getObjectId() != newNotification.getOrder().getObjectId()) {
-                jdbcTemplate.update(updateReference, newNotification.getOrder().getObjectId(), newNotification
-                        .getObjectId(), 27);
+                jdbcTemplate.update(updateReference, newNotification.getOrder().getObjectId(),
+                        newNotification.getObjectId(), 27);
             }
         } catch (NullPointerException e) {
             throw new TransactionException(newNotification);
--- src/main/java/projectpackage/repository/notificationsdao/NotificationTypeDAOImpl.java
+++ src/main/java/projectpackage/repository/notificationsdao/NotificationTypeDAOImpl.java
@@ -1,5 +1,7 @@
 package projectpackage.repository.notificationsdao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import projectpackage.model.notifications.NotificationType;
 import projectpackage.repository.AbstractDAO;
 import projectpackage.repository.daoexceptions.TransactionException;
@@ -7,15 +9,41 @@ import projectpackage.repository.daoexceptions.TransactionException;
 /**
  * Created by Arizel on 16.05.2017.
  */
-public class NotificationTypeDAOImpl extends AbstractDAO implements NotificationTypeDAO{
+public class NotificationTypeDAOImpl extends AbstractDAO implements NotificationTypeDAO {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
     @Override
     public int insertNotificationType(NotificationType notificationType) throws TransactionException {
-        return 0;
+        Integer objectId = nextObjectId();
+        try {
+            //11 = Notification_type
+            jdbcTemplate.update(insertObjects, objectId, null, 11, null, null);
+            //40 = Notification_type_title
+            jdbcTemplate.update(insertAttributes, 40, objectId, notificationType.getNotificationTypeTitle(), null);
+            //41 = Oriented
+            jdbcTemplate.update(insertObjReference, 41, objectId, notificationType.getOrientedRole().getObjectId());
+        } catch (NullPointerException e) {
+            throw new TransactionException(notificationType);
+        }
+        return objectId;
     }
 
     @Override
     public void updateNotificationType(NotificationType newNotificationType, NotificationType oldNotificationType) throws TransactionException {
-
+        try {
+            if (!oldNotificationType.getNotificationTypeTitle().equals(newNotificationType.getNotificationTypeTitle())) {
+                jdbcTemplate.update(updateAttributes, newNotificationType.getNotificationTypeTitle(), null,
+                        newNotificationType.getObjectId(), 40);
+            }
+            if (oldNotificationType.getOrientedRole().getObjectId() != newNotificationType.getOrientedRole().getObjectId()) {
+                jdbcTemplate.update(updateReference, newNotificationType.getOrientedRole().getObjectId(),
+                        newNotificationType.getObjectId(), 41);
+            }
+        } catch (NullPointerException e) {
+            throw new TransactionException(newNotificationType);
+        }
     }
 
     @Override
