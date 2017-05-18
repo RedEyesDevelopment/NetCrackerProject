37fc688b62c2fa0359653d8e3d3678211f5e0aa4
Sergey Alekseenko <alekseenko365@gmail.com>
2017-05-18 16:03:49 +0300

    NET CRACKER PROJECT. Version: 0.0.6 Branch: alekseenko_dao Date: 18.05.2017 Author: alekseenkoSergey
    Working: PriceDAO
    Changes: PriceDAO
    Next: merge with master
    
    Comment:

--- src/main/java/projectpackage/repository/ratesdao/PriceDAOImpl.java
+++ src/main/java/projectpackage/repository/ratesdao/PriceDAOImpl.java
@@ -1,5 +1,7 @@
 package projectpackage.repository.ratesdao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import projectpackage.model.rates.Price;
 import projectpackage.repository.AbstractDAO;
 import projectpackage.repository.daoexceptions.TransactionException;
@@ -8,14 +10,40 @@ import projectpackage.repository.daoexceptions.TransactionException;
  * Created by Arizel on 16.05.2017.
  */
 public class PriceDAOImpl extends AbstractDAO implements PriceDAO{
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
     @Override
     public int insertPrice(Price price) throws TransactionException {
-        return 0;
+        Integer objectId = nextObjectId();
+        try {
+            //7 = Price
+            jdbcTemplate.update(insertObjects, objectId, price.getRateId(), 7, null, null);
+            //32 = Number_of_people
+            jdbcTemplate.update(insertAttributes, 32, objectId, price.getNumberOfPeople(), null);
+            //33 = Rate
+            jdbcTemplate.update(insertAttributes, 33, objectId, price.getRate(), null);
+        } catch (NullPointerException e) {
+            throw new TransactionException(price);
+        }
+        return objectId;
     }
 
     @Override
     public void updatePrice(Price newPrice, Price oldPrice) throws TransactionException {
-
+        try {
+            if (!oldPrice.getNumberOfPeople().equals(newPrice.getNumberOfPeople())) {
+                jdbcTemplate.update(updateAttributes, newPrice.getNumberOfPeople(), null,
+                        newPrice.getObjectId(), 32);
+            }
+            if (!oldPrice.getRate().equals(newPrice.getRate())) {
+                jdbcTemplate.update(updateAttributes, newPrice.getRate(), null,
+                        newPrice.getObjectId(), 33);
+            }
+        } catch (NullPointerException e) {
+            throw new TransactionException(newPrice);
+        }
     }
 
     @Override
