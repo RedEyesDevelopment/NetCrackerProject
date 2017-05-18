b9789dd15adf6736ac7e33f92fb2c58b9c75c390
Sergey Alekseenko <alekseenko365@gmail.com>
2017-05-18 12:54:44 +0300

    NET CRACKER PROJECT. Version: 0.0.4 Branch: alekseenko_dao Date: 18.05.2017 Author: alekseenkoSergey
    Working: almost done NotificationTypeService (+tests) except getNotificationTypeByRole
    Changes: NotificationTypeService and NotificationTypeRepositoryTests
    Next: NotificationService
    
    Comment: не сделан метод getNotificationTypeByRole, и в методе deleteNotificationType надо вставить проверку нет ли Notification'ов такого типа (сделать после реализации getNotificationsByType).

--- src/main/java/projectpackage/service/notificationservice/NotificationTypeServiceImpl.java
+++ src/main/java/projectpackage/service/notificationservice/NotificationTypeServiceImpl.java
@@ -1,7 +1,9 @@
 package projectpackage.service.notificationservice;
 
+import lombok.extern.log4j.Log4j;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import projectpackage.model.auth.Role;
 import projectpackage.model.notifications.NotificationType;
 import projectpackage.repository.daoexceptions.TransactionException;
@@ -14,7 +16,9 @@ import java.util.List;
 /**
  * Created by Arizel on 16.05.2017.
  */
-public class NotificationTypeServiceImpl implements NotificationTypeService{
+@Log4j
+@Service
+public class NotificationTypeServiceImpl implements NotificationTypeService {
 
     private static final Logger LOGGER = Logger.getLogger(NotificationTypeServiceImpl.class);
 
@@ -27,8 +31,13 @@ public class NotificationTypeServiceImpl implements NotificationTypeService{
     @Override
     public List<NotificationType> getAllNotificationTypes() {
         List<NotificationType> list = null;
-        list = (List<NotificationType>) manager.createReactEAV(NotificationType.class)
-                .fetchInnerEntityCollection(Role.class).closeFetch();
+        try {
+            list = (List<NotificationType>) manager.createReactEAV(NotificationType.class)
+                    .fetchInnerEntityCollection(Role.class).closeFetch()
+                    .getEntityCollection();
+        } catch (ResultEntityNullException e) {
+            LOGGER.warn("getAllUsers method returned null list", e);
+        }
         return list;
     }
 
@@ -66,7 +75,20 @@ public class NotificationTypeServiceImpl implements NotificationTypeService{
 
     @Override
     public boolean deleteNotificationType(int id) {
-        return false;
+        NotificationType notificationType = null;
+        try {
+            notificationType = (NotificationType) manager.createReactEAV(NotificationType.class)
+                    .fetchInnerEntityCollection(Role.class).closeFetch()
+                    .getSingleEntityWithId(id);
+        } catch (ResultEntityNullException e) {
+            return false;
+        }
+        int count = 0;
+        /* todo надо проверить есть ли хоть один Notification этого типа
+           написать этот кусок, когда будет готово getNotificationsByType */
+        count = count + notificationTypeDAO.deleteNotificationType(id);
+        if (count == 0) return false;
+        else return true;
     }
 
     @Override
@@ -75,13 +97,24 @@ public class NotificationTypeServiceImpl implements NotificationTypeService{
             int notifTypeId = notificationTypeDAO.insertNotificationType(notificationType);
             LOGGER.info("Get from DB notificationTypeId = " + notifTypeId);
         } catch (TransactionException e) {
-            return false;
+            return false; // todo писать тут что-то в логгер
         }
         return true;
     }
 
     @Override
     public boolean updateNotificationType(int id, NotificationType newNotificationType) {
-        return false;
+        try {
+            newNotificationType.setObjectId(id);
+            NotificationType oldUser = (NotificationType) manager.createReactEAV(NotificationType.class)
+                    .fetchInnerEntityCollection(Role.class).closeFetch()
+                    .getSingleEntityWithId(newNotificationType.getObjectId());
+            notificationTypeDAO.updateNotificationType(newNotificationType, oldUser);
+        } catch (ResultEntityNullException e) {
+            return false;
+        } catch (TransactionException e) {
+            return false;
+        }
+        return true;
     }
 }
--- src/test/java/tests/database/NotificationTypeRepositoryTests.java
+++ src/test/java/tests/database/NotificationTypeRepositoryTests.java
@@ -1,57 +1,96 @@
 package tests.database;
 
+import org.apache.log4j.Logger;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.annotation.Rollback;
-import projectpackage.model.auth.Phone;
-import projectpackage.service.notificationservice.NotificationService;
+import projectpackage.model.auth.Role;
+import projectpackage.model.notifications.NotificationType;
+import projectpackage.service.notificationservice.NotificationTypeService;
+import projectpackage.service.notificationservice.NotificationTypeServiceImpl;
 
 import java.util.List;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 /**
  * Created by Arizel on 16.05.2017.
  */
-public class NotificationTypeRepositoryTests extends AbstractDatabaseTest{
+public class NotificationTypeRepositoryTests extends AbstractDatabaseTest {
+
+    private static final Logger LOGGER = Logger.getLogger(NotificationTypeServiceImpl.class);
 
     @Autowired
-    NotificationService notificationService;
+    NotificationTypeService notificationTypeService;
 
     @Test
     @Rollback(true)
     public void getAllNotificationTypes() {
-
+        List<NotificationType> list = notificationTypeService.getAllNotificationTypes("notif_type_title", true);
+        for (NotificationType notificationType : list) {
+            LOGGER.info(notificationType);
+            assertNotNull(notificationType);
+        }
+        LOGGER.info(SEPARATOR);
     }
 
     @Test
     @Rollback(true)
-    public void getSingleNotificationTypeById(){
-
+    public void getSingleNotificationTypeById() {
+        NotificationType notificationType = null;
+        int notifTypeId = 2000;
+        notificationType = notificationTypeService.getSingleNotificationTypeById(notifTypeId);
+        assertNotNull(notificationType);
+        LOGGER.info(notificationType);
+        LOGGER.info(SEPARATOR);
     }
 
     @Test
     @Rollback(true)
-    public void getNotificationTypeByRole(){
-
+    public void getNotificationTypeByRole() {
+        // todo
     }
 
 
     @Test
     @Rollback(true)
-    public void deleteNotificationType(){
-
+    public void deleteNotificationType() {
+        int notifTypeId = 2000;
+        boolean result = notificationTypeService.deleteNotificationType(notifTypeId);
+        assertTrue(result);
+        LOGGER.info("Delete notifType result = " + result);
+        LOGGER.info(SEPARATOR);
     }
 
     @Test
     @Rollback(true)
-    public void createNotificationType(){
-
+    public void createNotificationType() {
+        Role role = new Role();
+        role.setRoleName("Admin");
+        role.setObjectId(1);
+        NotificationType notificationType = new NotificationType();
+        notificationType.setNotificationTypeTitle("TestNotificationType");
+        notificationType.setOrientedRole(role);
+        boolean result = notificationTypeService.insertNotificationType(notificationType);
+        assertTrue(result);
+        LOGGER.info("Create notification result = " + result);
+        LOGGER.info(SEPARATOR);
     }
 
     @Test
     @Rollback(true)
-    public void updateNotificationType(){
-
+    public void updateNotificationType() {
+        Role newRole = new Role();
+        newRole.setObjectId(2);
+        newRole.setRoleName("Reception");
+        NotificationType notificationType = new NotificationType();
+        notificationType.setObjectId(2000);
+        notificationType.setOrientedRole(newRole);
+        notificationType.setNotificationTypeTitle("UpdateNotifTypeTEST");
+        boolean result = notificationTypeService.updateNotificationType(notificationType.getObjectId(), notificationType);
+        assertTrue(result);
+        LOGGER.info("Update notification result = " + result);
+        LOGGER.info(SEPARATOR);
     }
 }
