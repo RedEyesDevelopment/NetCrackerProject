75342fe031534a2773872836bd59729a0ec35b01
Dima <fiendes03@gmail.com>
2017-05-19 12:29:54 +0300

    NET CRACKER PROJECT. Version: 0.0.1 Branch: dudnik_dao_2 Date: 18.05.2017 Author: Fiendes
    Working:update of RateDAO and RateService
    Changes: RateDAO and RateService
    Next: tests and merge
    
    Comment:
     almost finished DAO

--- src/main/java/projectpackage/repository/ratesdao/RateDAOImpl.java
+++ src/main/java/projectpackage/repository/ratesdao/RateDAOImpl.java
@@ -1,21 +1,55 @@
 package projectpackage.repository.ratesdao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import projectpackage.model.rates.Rate;
 import projectpackage.repository.AbstractDAO;
 import projectpackage.repository.daoexceptions.TransactionException;
 
-/**
- * Created by Arizel on 16.05.2017.
- */
+
 public class RateDAOImpl extends AbstractDAO implements RateDAO{
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+
+
     @Override
     public int insertRate(Rate rate) throws TransactionException {
-        return 0;
+        Integer objectId = nextObjectId();
+        try {
+            //6 = Rate
+            jdbcTemplate.update(insertObjects, objectId, rate.getRoomTypeId(), 6, null, null);
+            //30 = Rate_from_date
+            jdbcTemplate.update(insertAttributes, 30, objectId, null, rate.getRateFromDate());
+            //31 = Rate_to_date
+            jdbcTemplate.update(insertAttributes, 31, objectId, null, rate.getRateToDate());
+            //44 = Creation_date
+            jdbcTemplate.update(insertAttributes, 44, objectId, null, rate.getCreationDate());
+        } catch (NullPointerException e) {
+            throw new TransactionException(rate);
+        }
+        return objectId;
     }
 
     @Override
     public void updateRate(Rate newRate, Rate oldRate) throws TransactionException {
-
+        try {
+            if (oldRate.getRateFromDate().getTime() != newRate.getRateFromDate().getTime()) {
+                jdbcTemplate.update(updateAttributes, null, newRate.getRateFromDate(),
+                        newRate.getObjectId(), 30);
+            }
+            if (oldRate.getRateToDate().getTime() != newRate.getRateToDate().getTime()) {
+                jdbcTemplate.update(updateAttributes, null, newRate.getRateToDate(),
+                        newRate.getObjectId(), 31);
+            }
+            if (oldRate.getCreationDate().getTime() != newRate.getCreationDate().getTime()) {
+                jdbcTemplate.update(updateAttributes, null, newRate.getCreationDate(),
+                        newRate.getObjectId(), 44);
+            }
+        } catch (NullPointerException e) {
+            throw new TransactionException(newRate);
+        }
     }
 
     @Override
--- src/main/java/projectpackage/service/rateservice/RateServiceImpl.java
+++ src/main/java/projectpackage/service/rateservice/RateServiceImpl.java
@@ -1,6 +1,16 @@
 package projectpackage.service.rateservice;
 
+import lombok.extern.log4j.Log4j;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import projectpackage.model.rates.Price;
 import projectpackage.model.rates.Rate;
+import projectpackage.model.rooms.RoomType;
+import projectpackage.repository.daoexceptions.TransactionException;
+import projectpackage.repository.ratesdao.PriceDAO;
+import projectpackage.repository.ratesdao.RateDAO;
+import projectpackage.repository.reacteav.ReactEAVManager;
+import projectpackage.repository.reacteav.exceptions.ResultEntityNullException;
 
 import java.util.List;
 
@@ -8,6 +18,18 @@ import java.util.List;
  * Created by Arizel on 16.05.2017.
  */
 public class RateServiceImpl implements RateService{
+    private static final Logger LOGGER = Logger.getLogger(RateServiceImpl.class);
+
+
+    @Autowired
+    RateDAO rateDAO;
+
+    @Autowired
+    PriceDAO priceDAO;
+
+    @Autowired
+    ReactEAVManager manager;
+
     @Override
     public List<Rate> getAllRates() {
         return null;
@@ -25,16 +47,54 @@ public class RateServiceImpl implements RateService{
 
     @Override
     public boolean deleteRate(int id) {
-        return false;
+        Rate rate = null;
+        try {
+            rate = (Rate) manager.createReactEAV(Rate.class).
+                    fetchInnerEntityCollection(Price.class).closeFetch().
+                    fetchInnerEntityCollection(RoomType.class).closeFetch().
+                    getSingleEntityWithId(id);
+        } catch (ResultEntityNullException e) {
+            return false;
+        }
+        int count = 0;
+        if (null != rate.getPrices()) {
+            for (Price price : rate.getPrices()) {
+                count = count + priceDAO.deletePrice(price.getObjectId());
+            }
+        }
+        count = count + rateDAO.deleteRate(id);
+        if (count == 0) return false;
+        else return true;
     }
 
     @Override
     public boolean insertRate(Rate rate) {
-        return false;
+        try {
+            int rateId = rateDAO.insertRate(rate);
+            LOGGER.info("Get from DB rateId = " + rateId);
+        } catch (TransactionException e) {
+            LOGGER.warn("Catched transactionException!!!", e);
+            return false;
+        }
+        return true;
     }
 
     @Override
     public boolean updateRate(int id, Rate newRate) {
-        return false;
+        try {
+            newRate.setObjectId(id);
+            Rate oldRate = (Rate) manager.createReactEAV(Rate.class).
+                    fetchInnerEntityCollection(Price.class).closeFetch().
+                    fetchInnerEntityCollection(RoomType.class).closeFetch().
+                    getSingleEntityWithId(newRate.getObjectId());
+            rateDAO.updateRate(newRate,oldRate);
+        } catch (ResultEntityNullException e) {
+            LOGGER.warn("Problem with ReactEAV! Pls Check!", e);
+            return false;
+        } catch (TransactionException e) {
+            LOGGER.warn("Catched transactionException!!!", e);
+            return false;
+        }
+        return true;
     }
 }
