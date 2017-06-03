d6ab919186bf068d77575cd38fa283cddde6e7da
Gvo3d <denis.yakimov89@gmail.com>
2017-06-03 21:41:40 +0300

    NET CRACKER PROJECT. Version: 0.0.84 Branch: master Date: 3.06.2017 Author: Gvo3d
    Working: security start
    Changes: merged with first_ui branch
    Next: all ui sht
    
    Comment: dunno

--- src/main/java/projectpackage/configuration/WebSecurityConfig.java
+++ src/main/java/projectpackage/configuration/WebSecurityConfig.java
@@ -37,7 +37,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/auth").permitAll()
                 .antMatchers("/auth/**").permitAll()
                 .antMatchers("/users").anonymous()
-                .antMatchers("/orders").permitAll()
+                .antMatchers("/orders/**").permitAll()
                 .antMatchers("/users/**").hasAnyAuthority("CLIENT", "ADMIN");
 
         http.requiresChannel().antMatchers("/auth/login").requiresSecure();
