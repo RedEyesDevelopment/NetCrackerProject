89801b7ac933750b26e79352bb0a8eff35bbfc94
Gvo3d <denis.yakimov89@gmail.com>
2017-06-08 14:33:15 +0300

    NET CRACKER PROJECT. Version: 0.1.11 Branch: master Date: 08.06.2017 Author: Gvozd
    Working: merge withsecurity_final
    Changes: merged
    Next: ui, deploy
    
    Comment: fuck yeah!

--- src/main/java/projectpackage/controllers/ClientController.java
+++ src/main/java/projectpackage/controllers/ClientController.java
@@ -3,10 +3,7 @@ package projectpackage.controllers;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import projectpackage.dto.IUDAnswer;
 import projectpackage.dto.MailDTO;
 
@@ -23,6 +20,6 @@ public class ClientController {
     public ResponseEntity<IUDAnswer> sendMessage(@RequestBody MailDTO mailDTO, HttpServletRequest request) {
         System.out.println(mailDTO);
         IUDAnswer iudAnswer = new IUDAnswer(true);
-        return new ResponseEntity<IUDAnswer>(iudAnswer, HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<IUDAnswer>(iudAnswer, HttpStatus.OK);
     }
 }
