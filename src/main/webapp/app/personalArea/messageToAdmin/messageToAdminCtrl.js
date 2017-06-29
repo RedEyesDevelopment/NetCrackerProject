462ddeca4a1776ccd034a89c548c2e12c511e1dd
Sergey Alekseenko <alekseenko365@gmail.com>
2017-06-29 13:23:54 +0300

    Version: 0.1.115 Branch: master Date: 29.06.2017 Author: alekseenkoSergey
    Working:
    Changes:
    Next:
    
    Comment:

---
+++ src/main/webapp/app/personalArea/messageToAdmin/messageToAdminCtrl.js
@@ -0,0 +1,41 @@
+app.controller('messageToAdminCtrl', ['$scope', '$http', '$location', 'sharedData',
+	function($scope, $http, $location, sharedData) {
+
+
+	$scope.mail = {}
+	$scope.stage = "looking";
+
+	$scope.prepareToSend = function() {
+		$scope.stage = "sending"
+	}
+
+	$scope.query = function() {
+		switch ($scope.stage) {
+			case 'sending': sendMessage();
+				break;
+		}
+	}
+
+	var sendMessage = function() {
+		$http({
+			url: sharedData.getLinks().https + '/clients/sendMessage',
+			method: 'POST',
+			data: {
+				themeMessage: $scope.mail.theme,
+				message: $scope.mail.message
+			},
+			headers: { 'Content-Type' : 'application/json' }
+		}).then(function(data) {
+			$scope.mail = {}
+			$scope.stage = "sended";
+		}, function(response) {
+			console.log("Smth wrong!!");
+			console.log(response);
+		});
+	}
+
+
+
+
+
+}]);
