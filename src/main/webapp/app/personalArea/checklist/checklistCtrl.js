cf10b5013a070936b97da0bf0f4f89d7b6f4402e
Dima <fiendes03@gmail.com>
2017-06-30 16:33:06 +0300

    NET CRACKER PROJECT. Version: 0.0.14 Branch: dudnik Date: 30.06.2017 Author: Fiendes
    Working:
    Changes:
    Next: Frontend
    
    Comment:

--- src/main/webapp/app/personalArea/checklist/checklistCtrl.js
+++ src/main/webapp/app/personalArea/checklist/checklistCtrl.js
@@ -1,29 +1,52 @@
 app.controller('checklistCtrl', ['$scope', '$http', '$location', 'sharedData',
 	function($scope, $http, $location, sharedData) {
 
+	/* Функция на получения всех заказов, вызываются сразу */
+    	(function() {
+    		$http({
+    			url: sharedData.getLinks().https + '/checklist',
+    			method: 'GET',
+    			headers: { 'Content-Type' : 'application/json' }
+    		}).then(function(data) {
+    			console.log(data);
+    			$scope.listOfOrders = data.data;
+    		}, function(response) {
+    			console.log("Smth wrong!!");
+    			console.log(response);
+    		});
+    	}());
 
+        $scope.checklist = {}
+        $scope.stage = "looking";
 
+        $scope.prepareToSend = function() {
+            $scope.stage = "sending"
+        }
 
+        $scope.query = function() {
+            switch ($scope.stage) {
+                case 'sending': sendMessage();
+                    break;
+            }
+        }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        var sendMessage = function() {
+            $http({
+                url: sharedData.getLinks().https + '/clients/sendMessage',
+                method: 'POST',
+                data: {
+                    themeMessage: $scope.mail.theme,
+                    message: $scope.mail.message
+                },
+                headers: { 'Content-Type' : 'application/json' }
+            }).then(function(data) {
+                $scope.mail = {}
+                $scope.stage = "sended";
+            }, function(response) {
+                console.log("Smth wrong!!");
+                console.log(response);
+            });
+        }
 
 
 
