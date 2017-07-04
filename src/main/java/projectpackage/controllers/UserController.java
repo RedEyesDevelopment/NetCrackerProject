724b78f48be94747e65cd6d19d3c53df28b57bbe
AlexMerlyan <sasha-merlyan@yandex.com>
2017-07-04 22:47:28 +0300

    Version: 0.0.3 Branch: merlyan_transactions Date: 04.07.2017 Author: AlexMerlyan
    Working: users)
    Changes:
    Next:
    
    Comment:

--- src/main/java/projectpackage/controllers/UserController.java
+++ src/main/java/projectpackage/controllers/UserController.java
@@ -1,5 +1,6 @@
 package projectpackage.controllers;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.Resource;
 import org.springframework.http.HttpStatus;
@@ -10,6 +11,9 @@ import projectpackage.dto.*;
 import projectpackage.model.auth.Phone;
 import projectpackage.model.auth.Role;
 import projectpackage.model.auth.User;
+import projectpackage.repository.support.daoexceptions.DeletedObjectNotExistsException;
+import projectpackage.repository.support.daoexceptions.DuplicateEmailException;
+import projectpackage.repository.support.daoexceptions.WrongEntityIdException;
 import projectpackage.service.authservice.UserService;
 import projectpackage.service.securityservice.SecurityService;
 import projectpackage.service.support.ServiceUtils;
@@ -28,7 +32,9 @@ import static projectpackage.service.MessageBook.*;
 @RequestMapping("/users")
 public class UserController {
 
-	@Autowired
+    private static final Logger LOGGER = Logger.getLogger(UserController.class);
+
+    @Autowired
 	UserService userService;
 
 	@Autowired
@@ -90,13 +96,16 @@ public class UserController {
         newUser.setFirstName(userDTO.getFirstName());
         newUser.setLastName(userDTO.getLastName());
         newUser.setEnabled(Boolean.TRUE);
-		IUDAnswer result = userService.insertUser(newUser);
+
+        iudAnswer = registerUser(newUser);
+
 		HttpStatus status;
-		if (result.isSuccessful()) {
+		if (iudAnswer != null && iudAnswer.isSuccessful()) {
 		    status = HttpStatus.OK;
-        } else status = HttpStatus.BAD_REQUEST;
-		ResponseEntity<IUDAnswer> responseEntity = new ResponseEntity<IUDAnswer>(result, status);
-		return responseEntity;
+        } else {
+		    status = HttpStatus.BAD_REQUEST;
+        }
+		return new ResponseEntity<IUDAnswer>(iudAnswer, status);
 	}
 
 	@RequestMapping(value = "/registration", method = RequestMethod.POST, produces = {MediaType
@@ -110,11 +119,13 @@ public class UserController {
         role.setRoleName("CLIENT");
         newUser.setRole(role);
         newUser.setEnabled(Boolean.TRUE);
-        IUDAnswer result = userService.insertUser(newUser);
+        IUDAnswer result = registerUser(newUser);
         HttpStatus status;
-        if (result.isSuccessful()) {
+        if (result != null && result.isSuccessful()) {
             status = HttpStatus.OK;
-        } else status = HttpStatus.BAD_REQUEST;
+        } else {
+            status = HttpStatus.BAD_REQUEST;
+        }
         ResponseEntity<IUDAnswer> responseEntity = new ResponseEntity<IUDAnswer>(result, status);
         return responseEntity;
 	}
@@ -142,12 +153,14 @@ public class UserController {
         changedUser.setPassword(userDTO.getPassword());
         changedUser.setRole(role);
         changedUser.setPhones(phones);
-		IUDAnswer result = userService.updateUser(id, changedUser);
+        iudAnswer = updateUser(id, user);
 		HttpStatus status;
-		if (result.isSuccessful()) {
+		if (iudAnswer != null && iudAnswer.isSuccessful()) {
 		    status = HttpStatus.OK;
-        } else status = HttpStatus.BAD_REQUEST;
-		ResponseEntity<IUDAnswer> responseEntity = new ResponseEntity<IUDAnswer>(result, status);
+        } else {
+		    status = HttpStatus.BAD_REQUEST;
+        }
+		ResponseEntity<IUDAnswer> responseEntity = new ResponseEntity<IUDAnswer>(iudAnswer, status);
 		return responseEntity;
 	}
 
@@ -161,14 +174,13 @@ public class UserController {
             return new ResponseEntity<IUDAnswer>(iudAnswer, HttpStatus.BAD_REQUEST);
         }
         User user = userService.getSingleUserById(id);
-
         if (!securityService.isPasswordMatchEncrypted(userPasswordDTO.getOldPassword(), user.getPassword())) {
             return new ResponseEntity<IUDAnswer>(new IUDAnswer(false, WRONG_PASSWORD), HttpStatus.BAD_REQUEST);
         }
-
         user.setPassword(userPasswordDTO.getNewPassword());
-        IUDAnswer answer = userService.updateUserPassword(id, user);
-        if (answer.isSuccessful()) {
+        IUDAnswer answer = changePassword(id, user);
+
+        if (answer != null && answer.isSuccessful()) {
             request.getSession().removeAttribute("USER");
             request.getSession().setAttribute("USER", userService.getSingleUserById(id));
             return new ResponseEntity<IUDAnswer>(answer, HttpStatus.OK);
@@ -176,6 +188,27 @@ public class UserController {
             return new ResponseEntity<IUDAnswer>(answer, HttpStatus.BAD_REQUEST);
         }
 	}
+
+    @RequestMapping(value = "admin/update/password/{id}", method = RequestMethod.PUT,
+            produces = {MediaType.APPLICATION_JSON_UTF8_VALUE}, consumes = {MediaType.APPLICATION_JSON_UTF8_VALUE})
+    public ResponseEntity<IUDAnswer> updateAdminPassword(@PathVariable("id") Integer id,
+                                                         @RequestBody UserPasswordDTO userPasswordDTO, HttpServletRequest request) {
+        User sessionUser = (User) request.getSession().getAttribute("USER");
+        IUDAnswer iudAnswer = serviceUtils.checkAdminForChangePassword(sessionUser, userPasswordDTO, id);
+        if (!iudAnswer.isSuccessful()) {
+            return new ResponseEntity<IUDAnswer>(iudAnswer, HttpStatus.BAD_REQUEST);
+        }
+
+        User user = userService.getSingleUserById(id);
+
+        if (!securityService.isPasswordMatchEncrypted(userPasswordDTO.getOldPassword(), user.getPassword())) {
+            return new ResponseEntity<IUDAnswer>(new IUDAnswer(false, WRONG_PASSWORD), HttpStatus.BAD_REQUEST);
+        }
+
+        user.setPassword(userPasswordDTO.getNewPassword());
+        IUDAnswer answer = changePassword(id, user);
+        return checkUpdateForAdmin(answer, request, sessionUser, id);
+    }
 
     @RequestMapping(value = "admin/update/role/{id}", method = RequestMethod.PUT,
             produces = {MediaType.APPLICATION_JSON_UTF8_VALUE}, consumes = {MediaType.APPLICATION_JSON_UTF8_VALUE})
@@ -196,33 +229,14 @@ public class UserController {
         Role role = new Role();
         role.setObjectId(roleDTO.getRoleId());
         user.setRole(role);
-        IUDAnswer answer = userService.updateUser(id, user);
-        if (answer.isSuccessful()) {
-            return new ResponseEntity<IUDAnswer>(answer, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<IUDAnswer>(answer, HttpStatus.BAD_REQUEST);
-        }
-    }
 
-    @RequestMapping(value = "admin/update/password/{id}", method = RequestMethod.PUT,
-            produces = {MediaType.APPLICATION_JSON_UTF8_VALUE}, consumes = {MediaType.APPLICATION_JSON_UTF8_VALUE})
-    public ResponseEntity<IUDAnswer> updateAdminPassword(@PathVariable("id") Integer id,
-                                                    @RequestBody UserPasswordDTO userPasswordDTO, HttpServletRequest request) {
-        User sessionUser = (User) request.getSession().getAttribute("USER");
-        IUDAnswer iudAnswer = serviceUtils.checkAdminForChangePassword(sessionUser, userPasswordDTO, id);
-        if (!iudAnswer.isSuccessful()) {
+        iudAnswer = updateUser(id, user);
+
+        if (iudAnswer != null && iudAnswer.isSuccessful()) {
+            return new ResponseEntity<IUDAnswer>(iudAnswer, HttpStatus.OK);
+        } else {
             return new ResponseEntity<IUDAnswer>(iudAnswer, HttpStatus.BAD_REQUEST);
         }
-
-        User user = userService.getSingleUserById(id);
-
-        if (!securityService.isPasswordMatchEncrypted(userPasswordDTO.getOldPassword(), user.getPassword())) {
-            return new ResponseEntity<IUDAnswer>(new IUDAnswer(false, WRONG_PASSWORD), HttpStatus.BAD_REQUEST);
-        }
-
-        user.setPassword(userPasswordDTO.getNewPassword());
-        IUDAnswer answer = userService.updateUserPassword(id, user);
-        return checkUpdateForAdmin(answer, request, sessionUser, id);
     }
 
 	@RequestMapping(value = "/update/basic/{id}", method = RequestMethod.PUT,
@@ -240,8 +254,8 @@ public class UserController {
 		user.setLastName(userBasicDTO.getLastName());
 		user.setEmail(userBasicDTO.getEmail());
 		user.setAdditionalInfo(userBasicDTO.getAdditionalInfo());
-		IUDAnswer answer = userService.updateUser(id, user);
-		if (answer.isSuccessful()) {
+		IUDAnswer answer = updateUser(id, user);
+		if (answer != null && answer.isSuccessful()) {
 			request.getSession().removeAttribute("USER");
 			request.getSession().setAttribute("USER", userService.getSingleUserById(id));
 			return new ResponseEntity<IUDAnswer>(answer, HttpStatus.OK);
@@ -263,7 +277,7 @@ public class UserController {
 		user.setLastName(userBasicDTO.getLastName());
 		user.setEmail(userBasicDTO.getEmail());
 		user.setAdditionalInfo(userBasicDTO.getAdditionalInfo());
-		IUDAnswer answer = userService.updateUser(id, user);
+		IUDAnswer answer = updateUser(id, user);
 		return checkUpdateForAdmin(answer, request, sessionUser, id);
 	}
 
@@ -275,14 +289,28 @@ public class UserController {
 		if (!iudAnswer.isSuccessful()) {
 		    return new ResponseEntity<IUDAnswer>(iudAnswer, HttpStatus.BAD_REQUEST);
         }
-		IUDAnswer result = userService.deleteUser(id);
+
+        try {
+            iudAnswer = userService.deleteUser(id);
+        } catch (DeletedObjectNotExistsException e) {
+            LOGGER.warn(RESTORED_USER_NOT_EXISTS, e);
+            return new ResponseEntity<IUDAnswer>(new IUDAnswer(id, false, RESTORED_USER_NOT_EXISTS, e.getMessage()), HttpStatus.BAD_REQUEST);
+        } catch (IllegalArgumentException e) {
+            LOGGER.warn(NULL_ID, e);
+            return new ResponseEntity<IUDAnswer>(new IUDAnswer(id, false, NULL_ID, e.getMessage()), HttpStatus.BAD_REQUEST);
+        } catch (WrongEntityIdException e) {
+            LOGGER.warn(WRONG_RESTORED_ID, e);
+            return new ResponseEntity<IUDAnswer>(new IUDAnswer(id, false, WRONG_RESTORED_ID, e.getMessage()), HttpStatus.BAD_REQUEST);
+        }
+
 		HttpStatus status;
-		if (result.isSuccessful()) {
+		if (iudAnswer != null && iudAnswer.isSuccessful()) {
 			status = HttpStatus.OK;
 		} else {
 			status = HttpStatus.NOT_FOUND;
 		}
-		ResponseEntity<IUDAnswer> responseEntity = new ResponseEntity<IUDAnswer>(result, status);
+
+		ResponseEntity<IUDAnswer> responseEntity = new ResponseEntity<IUDAnswer>(iudAnswer, status);
 		return responseEntity;
 	}
 
@@ -293,14 +321,26 @@ public class UserController {
         if (!iudAnswer.isSuccessful()) {
             return new ResponseEntity<IUDAnswer>(iudAnswer, HttpStatus.BAD_REQUEST);
         }
-        IUDAnswer result = userService.restoreUser(id);
+
+        try {
+            iudAnswer = userService.restoreUser(id);
+        } catch (DeletedObjectNotExistsException e) {
+            LOGGER.warn(RESTORED_USER_NOT_EXISTS, e);
+            return new ResponseEntity<IUDAnswer>(new IUDAnswer(id, false, RESTORED_USER_NOT_EXISTS, e.getMessage()), HttpStatus.BAD_REQUEST);
+        } catch (IllegalArgumentException e) {
+            LOGGER.warn(NULL_ID, e);
+            return new ResponseEntity<IUDAnswer>(new IUDAnswer(id, false, NULL_ID, e.getMessage()), HttpStatus.BAD_REQUEST);
+        } catch (WrongEntityIdException e) {
+            LOGGER.warn(WRONG_RESTORED_ID, e);
+            return new ResponseEntity<IUDAnswer>(new IUDAnswer(id, false, WRONG_RESTORED_ID, e.getMessage()), HttpStatus.BAD_REQUEST);
+        }
         HttpStatus status;
-        if (result.isSuccessful()) {
+        if (iudAnswer != null && iudAnswer.isSuccessful()) {
             status = HttpStatus.OK;
         } else {
             status = HttpStatus.NOT_FOUND;
         }
-        ResponseEntity<IUDAnswer> responseEntity = new ResponseEntity<IUDAnswer>(result, status);
+        ResponseEntity<IUDAnswer> responseEntity = new ResponseEntity<IUDAnswer>(iudAnswer, status);
         return responseEntity;
     }
 
@@ -319,6 +359,42 @@ public class UserController {
 
 		return new ResponseEntity<User>(user, status);
 	}
+
+	private IUDAnswer updateUser(Integer id, User user) {
+        try {
+            return userService.updateUser(id, user);
+        } catch (IllegalArgumentException e) {
+            LOGGER.warn(WRONG_FIELD, e);
+            return new IUDAnswer(id, false, WRONG_FIELD, e.getMessage());
+        } catch (DuplicateEmailException e) {
+            LOGGER.warn(DUPLICATE_EMAIL, e);
+            return new IUDAnswer(id, false, DUPLICATE_EMAIL, e.getMessage());
+        }
+    }
+
+	private IUDAnswer registerUser(User user) {
+        try {
+            return userService.insertUser(user);
+        } catch (IllegalArgumentException e) {
+            LOGGER.warn(WRONG_FIELD, e);
+            return new IUDAnswer(false, WRONG_FIELD, e.getMessage());
+        } catch (DuplicateEmailException e) {
+            LOGGER.warn(DUPLICATE_EMAIL, e);
+            return new IUDAnswer(false, DUPLICATE_EMAIL, e.getMessage());
+        }
+    }
+
+    private IUDAnswer changePassword(Integer id, User user) {
+        try {
+            return userService.updateUserPassword(id, user);
+        } catch (IllegalArgumentException e) {
+            LOGGER.warn(WRONG_FIELD, e);
+            return new IUDAnswer(id, false, WRONG_FIELD, e.getMessage());
+        } catch (DuplicateEmailException e) {
+            LOGGER.warn(DUPLICATE_EMAIL, e);
+            return new IUDAnswer(id, false, DUPLICATE_EMAIL, e.getMessage());
+        }
+    }
 
 	private ResponseEntity<IUDAnswer> checkUpdateForAdmin(IUDAnswer answer, HttpServletRequest request, User sessionUser, Integer id) {
         if (answer.isSuccessful() && sessionUser.getObjectId() == id.intValue()) {
--- src/main/java/projectpackage/service/authservice/UserServiceImpl.java
+++ src/main/java/projectpackage/service/authservice/UserServiceImpl.java
@@ -4,13 +4,13 @@ import lombok.extern.log4j.Log4j;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import projectpackage.dto.IUDAnswer;
 import projectpackage.model.auth.Phone;
 import projectpackage.model.auth.User;
 import projectpackage.repository.authdao.PhoneDAO;
 import projectpackage.repository.authdao.UserDAO;
 import projectpackage.repository.support.daoexceptions.DeletedObjectNotExistsException;
-import projectpackage.repository.support.daoexceptions.DuplicateEmailException;
 import projectpackage.repository.support.daoexceptions.WrongEntityIdException;
 import projectpackage.service.regex.RegexService;
 import projectpackage.service.securityservice.SecurityService;
@@ -35,6 +35,7 @@ public class UserServiceImpl implements UserService {
 	@Autowired
 	SecurityService securityService;
 
+	@Transactional(readOnly = true)
 	@Override
 	public List<User> getAllUsers() {
 		List<User> users = userDAO.getAllUsers();
@@ -44,6 +45,7 @@ public class UserServiceImpl implements UserService {
 		return users;
 	}
 
+	@Transactional(readOnly = true)
 	@Override
 	public User getSingleUserById(Integer id) {
 		User user = userDAO.getUser(id);
@@ -53,6 +55,7 @@ public class UserServiceImpl implements UserService {
 		return user;
 	}
 
+	@Transactional(readOnly = true)
 	@Override
 	public User getSingleUserByUsername(String username) {
 		if (null == username){
@@ -61,6 +64,7 @@ public class UserServiceImpl implements UserService {
 		return userDAO.getUserByUsername(username);
 	}
 
+	@Transactional
 	@Override
 	public IUDAnswer deleteUser(Integer id) {
         if (id == null) {
@@ -82,34 +86,25 @@ public class UserServiceImpl implements UserService {
         return new IUDAnswer(id, true);
 	}
 
+	@Transactional
 	@Override
 	public IUDAnswer restoreUser(Integer id) {
 		if (id == null) {
 			return new IUDAnswer(false, NULL_ID);
 		}
-		try {
-			userDAO.restoreUser(id);
-		} catch (DeletedObjectNotExistsException e) {
-			LOGGER.warn(RESTORED_USER_NOT_EXISTS, e);
-			return new IUDAnswer(id, false, RESTORED_USER_NOT_EXISTS, e.getMessage());
-		} catch (IllegalArgumentException e) {
-			LOGGER.warn(NULL_ID, e);
-			return new IUDAnswer(id, false, NULL_ID, e.getMessage());
-		} catch (WrongEntityIdException e) {
-			LOGGER.warn(WRONG_RESTORED_ID, e);
-			return new IUDAnswer(id, false, WRONG_RESTORED_ID, e.getMessage());
-		}
-		userDAO.commit();
+        userDAO.restoreUser(id);
+
 		return new IUDAnswer(id, true);
 	}
 
+	@Transactional
 	@Override
 	public IUDAnswer insertUser(User user) {
 		if (user == null) {
-			return null;
+			return new IUDAnswer(false, NULL_ENTITY);
 		}
 		if (user.getPhones() == null) {
-			return null;
+			return new IUDAnswer(false, NULL_ENTITY);
 		}
         for (Phone phone : user.getPhones()) {
             boolean isValid = regexService.isValidPhone(phone.getPhoneNumber());
@@ -121,53 +116,34 @@ public class UserServiceImpl implements UserService {
         	return new IUDAnswer(false, INVALID_PASSWORD);
 		}
 
-		Integer userId = null;
-		try {
-			userId = userDAO.insertUser(user);
-		} catch (IllegalArgumentException e) {
-			LOGGER.warn(WRONG_FIELD, e);
-			return new IUDAnswer(false, WRONG_FIELD, e.getMessage());
-		} catch (DuplicateEmailException e) {
-			LOGGER.warn(DUPLICATE_EMAIL, e);
-			return new IUDAnswer(false, DUPLICATE_EMAIL, e.getMessage());
+		Integer userId = userDAO.insertUser(user);
+
+		for (Phone phone : user.getPhones()) {
+			phone.setUserId(userId);
+			phone.setObjectId(phoneDAO.insertPhone(phone));
 		}
 
-        try {
-            for (Phone phone : user.getPhones()) {
-                phone.setUserId(userId);
-                phone.setObjectId(phoneDAO.insertPhone(phone));
-            }
-        } catch (IllegalArgumentException e) {
-			LOGGER.warn(WRONG_FIELD, e);
-			return new IUDAnswer(false, WRONG_FIELD, e.getMessage());
-        }
-		phoneDAO.commit();
 		return new IUDAnswer(userId, true);
 	}
 
+	@Transactional
 	@Override
 	public IUDAnswer updateUser(Integer id, User newUser) {
 	    if (newUser == null) {
-	    	return null;
+            return new IUDAnswer(false, NULL_ENTITY);
 		}
         if (id == null) {
 	    	return new IUDAnswer(false, NULL_ID);
 		}
-		try {
-			newUser.setObjectId(id);
-			User oldUser = userDAO.getUser(id);
-			userDAO.updateUser(newUser, oldUser);
-		} catch (IllegalArgumentException e) {
-			LOGGER.warn(WRONG_FIELD, e);
-			return new IUDAnswer(id, false, WRONG_FIELD, e.getMessage());
-		} catch (DuplicateEmailException e) {
-			LOGGER.warn(DUPLICATE_EMAIL, e);
-			return new IUDAnswer(id, false, DUPLICATE_EMAIL, e.getMessage());
-		}
-		userDAO.commit();
+
+        newUser.setObjectId(id);
+        User oldUser = userDAO.getUser(id);
+        userDAO.updateUser(newUser, oldUser);
+
 		return new IUDAnswer(id, true);
 	}
 
+	@Transactional
 	@Override
 	public IUDAnswer updateUserPassword(Integer id, User newUser) {
 		if (newUser == null) {
@@ -179,18 +155,10 @@ public class UserServiceImpl implements UserService {
 		if (!securityService.cryptUserPass(newUser)) {
 			return new IUDAnswer(false, FAIL_CRYPT_PASSWORD);
 		}
-		try {
-			newUser.setObjectId(id);
-			User oldUser = userDAO.getUser(id);
-			userDAO.updateUserPassword(newUser, oldUser);
-		} catch (IllegalArgumentException e) {
-			LOGGER.warn(WRONG_FIELD, e);
-			return new IUDAnswer(id, false, WRONG_FIELD, e.getMessage());
-		} catch (DuplicateEmailException e) {
-			LOGGER.warn(DUPLICATE_EMAIL, e);
-			return new IUDAnswer(id, false, DUPLICATE_EMAIL, e.getMessage());
-		}
-		userDAO.commit();
+        newUser.setObjectId(id);
+        User oldUser = userDAO.getUser(id);
+        userDAO.updateUserPassword(newUser, oldUser);
+
 		return new IUDAnswer(id, true);
 	}
 
